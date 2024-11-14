ab6984dc2895695cfbc72ce74e39c2e9d22e850d
Andrej Dortmann <andrej.dortmann25@gmail.com>
2024-11-14 11:55:25 +0100

    feat: add logic app

---
+++ src/interfaces/IPokemonJSON.ts
@@ -0,0 +1,11 @@
+export interface IPokemonJSON {
+    count:    number;
+    next:     string;
+    previous: null;
+    results:  Result[];
+}
+
+export interface Result {
+    name: string;
+    url:  string;
+}
